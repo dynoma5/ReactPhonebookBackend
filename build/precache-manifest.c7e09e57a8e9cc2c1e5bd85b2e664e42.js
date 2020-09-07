c53040719e6e742ea1157a2d5e75f85462ef0360
David Seo <manakete659@gmail.com>
2020-09-07 15:52:16 -0400

    Added front end build

---
+++ build/precache-manifest.c7e09e57a8e9cc2c1e5bd85b2e664e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddccdd4a5e2c0b8c834a53ae4d38359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c17b512a3024fe0cb27",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c17b512a3024fe0cb27",
+    "url": "/static/js/main.0b586b91.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
